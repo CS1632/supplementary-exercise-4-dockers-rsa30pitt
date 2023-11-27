15332bb989b43f6bf6e744fa8136d82ca03919ad
Rakan Al-Radady <90066220+rsa30pitt@users.noreply.github.com>
2023-11-27 17:16:24 -0500

    Added headless driver to D3Test

--- selenium/src/test/java/edu/pitt/cs/D3Test.java
+++ selenium/src/test/java/edu/pitt/cs/D3Test.java
@@ -36,12 +36,11 @@ public class D3Test {
     JavascriptExecutor js;
     @Before
     public void setUp() {
-      driver = new ChromeDriver();
-      js = (JavascriptExecutor) driver;
-      vars = new HashMap<String, Object>();
       ChromeOptions options = new ChromeOptions();
       options.addArguments("--headless");
       driver = new ChromeDriver(options);
+      js = (JavascriptExecutor) driver;
+      vars = new HashMap<String, Object>();
     }
     @After
     public void tearDown() {
